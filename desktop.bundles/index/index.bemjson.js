992406ae6cf704f04b453f21be0b1348c94b346e
Varvara Stepanova <toivonen@toivonen.ru>
2014-02-03 21:20:48 +0100

    Display users in cycle

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -14,15 +14,48 @@
                 {
                     block: 'users-table',
                     head: [
-                        'Username',
-                        'Comments',
-                        'Topics',
-                        'Usergroup',
-                        'Registratin date',
-                        'Last login',
-                        'Last comment'
+                        {
+                            id: 'name',
+                            title: 'Username'
+                        },
+                        {
+                            id: 'comments',
+                            title: 'Comments'
+                        },
+                        {
+                            id: 'topics',
+                            title: 'Topics'
+                        },
+                        {
+                            id: 'userGroup',
+                            title: 'Usergroup',
+                        },
+                        {
+                            id: 'regDate',
+                            title: 'Registration date'
+                        },
+                        {
+                            id: 'lastLogin',
+                            title: 'Last login'
+                        },
+                        {
+                            id: 'lastComment',
+                            title: 'Last comment'
+                        }
+                    ],
+                    users: [
+                        {
+                            id: 1,
+                            name: 'Amber H',
+                            upic: 'data/upics/25x25/1.jpg',
+                            comments: 64,
+                            topics: 9,
+                            userGroup: 'Registered user',
+                            regDate: '19-mar-12 12:00',
+                            lastLogin: '16-aug-12 21:11',
+                            lastComment: '16-aug-12 16:10'
+                        }
                     ]
-
                 }
             ]
         },
