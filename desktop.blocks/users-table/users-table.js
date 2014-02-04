6d6c3742656b2ae3ad139fd502ad2a5a4df6557f
Varvara Stepanova <toivonen@toivonen.ru>
2014-02-04 21:24:08 +0100

    Select and undoselect the rows after checking the head checkbox

--- desktop.blocks/users-table/users-table.js
+++ desktop.blocks/users-table/users-table.js
@@ -2,9 +2,27 @@ modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
 
 DOM.decl('users-table', {
 
+    onElemSetMod: {
+        'row' : {
+            'current' : function(elem, modName, modVal, prevModVal) {
+                var ch = this.findBlockInside(elem, 'checkbox');
+                ch.setMod('checked', modVal);
+            }
+        }
+    },
+
     toggleRow: function(e) {
-        var row = $(e.target.domElem).closest(this.buildSelector('tr'));
-        this.toggleMod(row, 'current', true);
+        var checkbox = $(e.target.domElem).bem('checkbox');
+        var isInHead = checkbox.domElem.closest(this.buildSelector('head-row')).length;
+        if (isInHead) {
+            this.toggleAll(checkbox.hasMod('checked'));
+        } else {
+            var row = checkbox.domElem.closest(this.buildSelector('row'));
+            this.setMod(row, 'current', checkbox.hasMod('checked'));
+        }
+    },
+    toggleAll: function(state) {
+        this.setMod(this.elem('row'), 'current', state);
     }
 
 }, {
