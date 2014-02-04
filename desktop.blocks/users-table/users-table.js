071a2ffaa81a6aa4038203734765e47998e7a237
Varvara Stepanova <toivonen@toivonen.ru>
2014-02-04 20:42:22 +0100

    Toggling a table row as current when checking a corresponding checkbox

--- desktop.blocks/users-table/users-table.js
+++ desktop.blocks/users-table/users-table.js
@@ -1,16 +1,17 @@
-modules.define('i-bem__dom', function(provide, DOM) {
+modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
 
 DOM.decl('users-table', {
 
-    toggleRow: function() {
-        console.log('Toggle Row');
+    toggleRow: function(e) {
+        var row = $(e.target.domElem).closest(this.buildSelector('tr'));
+        this.toggleMod(row, 'current', true);
     }
 
 }, {
 
     live : function() {
-        this.liveInitOnBlockInsideEvent('change', 'checkbox', function() {
-            this.toggleRow();
+        this.liveInitOnBlockInsideEvent('change', 'checkbox', function(e) {
+            this.toggleRow(e);
         });
     }
 
