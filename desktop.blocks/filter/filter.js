ee9b81f1e75ad0bc6b128639b579a7364500c44e
Varya Stepanova <mail@varya.me>
2014-02-07 14:44:12 +0100

    Create new select in comments are chosen

--- desktop.blocks/filter/filter.js
+++ desktop.blocks/filter/filter.js
@@ -1,13 +1,47 @@
-modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
+modules.define('i-bem__dom', ['jquery', 'BEMHTML'], function(provide, $, BEMHTML, DOM) {
 
 DOM.decl('filter', {
 
     _onSelect: function(e, data) {
         var select = e.target.domElem,
             row = select.closest(this.buildSelector('row')),
-            td = row.find(this.buildSelector('val'));
+            td = row.find(this.buildSelector('val')),
+            newSelect;
 
-        td[0].innerHTML = 'test';
+        if (data.value === "comments") {
+            newSelect = {
+                block: 'select',
+                name: 'field',
+                options: [
+                    {
+                        elem: 'option',
+                        value: '',
+                        content: 'bigger than'
+                    },
+                    {
+                        elem: 'option',
+                        value: '10',
+                        content: '10'
+                    },
+                    {
+                        elem: 'option',
+                        value: '20',
+                        content: '20'
+                    },
+                    {
+                        elem: 'option',
+                        value: '30',
+                        content: '30'
+                    }
+                ]
+            }
+        }
+
+        if (newSelect) {
+            DOM.update(td, BEMHTML.apply(newSelect));
+        }
+
+
     }
 
 }, {
